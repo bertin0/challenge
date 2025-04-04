1ef04712a235c765ccff9918f29497fb72b97bb2
Albert Geantă <albertgeanta@gmail.com>
2025-04-04 17:22:34 +0300

    Add filters with imageproc (slow, not inplace, CPU bound bleh)

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,14 @@
 use std::{io::BufRead, time::Instant};
 
 use egui::ColorImage;
+use image::{
+    Rgb,
+    imageops::{FilterType, resize},
+};
+use imageproc::{
+    geometric_transformations::{Interpolation, Projection, warp},
+    map::map_colors,
+};
 use nokhwa::{
     Camera, NokhwaError,
     pixel_format::RgbFormat,
@@ -46,18 +54,32 @@ impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         let frame = self.camera.frame().expect("Cannot get frame");
 
-        let mut decoded = frame
+        let decoded = frame
             .decode_image::<RgbFormat>()
             .expect("Cannot decode frame");
 
         let now = Instant::now();
 
+        let decoded = map_colors(&decoded, |p| Rgb([255 - p[0], 255 - p[1], 255 - p[2]]));
+        let decoded = warp(
+            &decoded,
+            &(Projection::from_matrix([1.0, 0.0, 0.0, 0.0, -1.0, 0.0, 0.0, 0.0, 1.0]).unwrap()
+                * Projection::translate(0.0, decoded.height() as f32 * -1.0)),
+            Interpolation::Bilinear,
+            Rgb([0, 0, 0]),
+        );
+        let decoded = resize(
+            &decoded,
+            decoded.width() / 2,
+            decoded.height() / 2,
+            FilterType::Nearest,
+        );
+        println!("Effects time: {:#?}", now.elapsed());
+
         let image = egui::ColorImage::from_rgb(
             [decoded.width() as usize, decoded.height() as usize],
             &decoded.as_raw(),
         );
-
-        println!("Create image time: {:#?}", now.elapsed());
 
         self.texture.set(image, egui::TextureOptions::default());
 
